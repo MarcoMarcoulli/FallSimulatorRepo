c4b7513ee3d14222830123879b0544f97e70e203
MarcoMarcoulli <loca.marco02@gmail.com>
2024-10-02 15:48:20 +0200

    Update Interface.java

--- Code/FallSimulatorProject/src/main/java/ingdelsw/FallSimulatorProject/Interface.java
+++ Code/FallSimulatorProject/src/main/java/ingdelsw/FallSimulatorProject/Interface.java
@@ -1,9 +1,19 @@
 package ingdelsw.FallSimulatorProject;
 
-public class Interface
-{
-    public static void main( String[] args )
-    {
-        
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Interface extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
+
