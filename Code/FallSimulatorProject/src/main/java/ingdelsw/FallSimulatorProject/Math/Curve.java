3c367e6f523fb30be13b21027f5e754d999e7a2c
MarcoMarcoulli <loca.marco02@gmail.com>
2024-10-04 00:03:55 +0200

    Creating Curve abstract class

--- Code/FallSimulatorProject/src/main/java/ingdelsw/FallSimulatorProject/Math/Curve.java
+++ Code/FallSimulatorProject/src/main/java/ingdelsw/FallSimulatorProject/Math/Curve.java
@@ -1,9 +1,15 @@
 package ingdelsw.FallSimulatorProject.Math;
 
-public class Curve {
-
+public abstract class Curve {
+	
+	// Metodo astratto per calcolare un punto sulla curva dato un parametro t
+	public abstract Point calcolaPunto(double t);
+	
+	// Metodo per ottenere la lunghezza della curva 
+    public abstract double lunghezza();
+	
 	public Curve() {
-		// TODO Auto-generated constructor stub
+		
 	}
 
 }
